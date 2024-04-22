5a66ba0a7ef8983bd61e0abdfa826bcf698bd84e
2057195 <efremselihom@gmail.com>
2024-04-22 15:13:38 -0400

    add a new line, println in the main()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,4 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("This is a toy project");
+        System.out.println("Using Git Bash gor VC");
+    }
 }
